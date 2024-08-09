7606b661c3da7273d4cd5fbacdc5fbef70134f17
FaridFn78 <faridxzr@gmail.com>
2024-08-09 11:02:53 +0700

    Mengirim pesan contact form ke google spreadsheets + dropdown menu

--- dist/JS/main.js
+++ dist/JS/main.js
@@ -18,6 +18,27 @@ hamburger.addEventListener("click", function () {
   navbar.classList.toggle("hidden");
 });
 
+// Dropdown Menu
+const dropdownBtn = document.getElementById("dropdown-menu");
+const dropdownList = document.getElementById("dropdown-list");
+
+// Ketika tombol dropdown(tombol More) di-klik
+dropdownBtn.addEventListener("click", function () {
+  /*
+ Hilangkan class hidden jika ada,
+ atau
+ tambahkan class hidden jika tidak ada.
+ */
+  dropdownList.classList.toggle("hidden");
+
+  /*
+ Tambahkan class show jika tidak ada,
+ atau
+ hilangkan class show jika ada.
+ */
+  dropdownList.classList.toggle("show");
+});
+
 // Tombol untuk kembali ke atas
 const toTopBtn = document.getElementById("toTopBtn");
 const popupText = document.querySelector(".popup-text");
@@ -40,4 +61,43 @@ darkmodeBtn.addEventListener("click", function () {
   hapus class dark jika tombol darkmode belum checked
   */
   darkmodeBtn.checked ? html.classList.add("dark") : html.classList.remove("dark");
+});
+
+// Contact Form
+const scriptURL = "https://script.google.com/macros/s/AKfycbzeLvINF75l4LR-HOaZxQEWjPPT9sOqcdtLg44Ugo95dxXIj0ZeHA1HJ292RXvbv92EbQ/exec";
+const form = document.forms["FaridFn-contact-form"];
+
+// Ketika Form dikirim
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  // Validasi
+  const inputName = form.name.value;
+  const inputEmail = form.email.value;
+  const inputMessage = form.message.value;
+
+  if (!inputName || !inputEmail || !inputMessage) {
+    alert("Please fill in all fields");
+    return;
+  }
+
+  fetch(scriptURL, { method: "POST", body: new FormData(form) })
+    .then((response) => {
+      console.log("Success!", response);
+
+      const popupText = document.getElementById("popup-form");
+      const popupFormBtn = document.getElementById("btn-close");
+
+      // Munculkan pesan popup berhasil dikirim
+      popupText.classList.remove("hidden");
+
+      // Ketika tombol close pada popup form diklik
+      popupFormBtn.addEventListener("click", function () {
+        // Hilangkan pesan popup
+        popupText.classList.add("hidden");
+      });
+    })
+    .catch((error) => console.error("Error!", error.message));
+
+  form.reset();
 });
